7fe1dd7a4bb09f3c717331338edc44ca34525021
Anna <anna.holzheuser@gmail.com>
2020-05-21 09:01:03 +0200

    Updated gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  //const test = {'grant_type':'','username' : 'post1mantest','password' : 'pw123', 'scope':'','client_id':'', 'client_secret':''}
+  var myToken = ""
+  const userAction = async () => {
+    const response = await fetch('http://server.bykovski.de:8000/users/token', {
+      method: 'POST',
+      headers: {
+        "Accept" :"application/json"  ,
+        "Content-Type" : 'application/x-www-form-urlencoded'
+    },
+      body: JSON.stringify("&username=post1mantest&password=pw123&"),
+      
+  });
+    const myJson = await response.json(); //extract JSON from the http response
+    // do something with myJson
+    myToken = myJson.access_token
+
+    console.log('ws://server.bykovski.de:8000/game/click?token=\"'+ myToken +'\"')
+  }
+  const socket = new WebSocket('ws://server.bykovski.de:8000/game/click?token=\"'+ myToken +'\"');
+
+  socket.onopen=function(event){
+    socket.send("Hey Server");
+  };
+
+// Connection opened
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+
+  userAction()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="src/App">
+      <button onClick={()=> userAction()}>click mich pls</button>
+      
     </div>
-  );
+  )
 }
 
 export default App;
